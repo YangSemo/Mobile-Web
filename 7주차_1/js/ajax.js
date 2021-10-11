4fbd34836f62866fbcd51d19c60efdb47fd0ece8
YangSemo1 <tpah20@gmail.com>
2021-10-11 11:03:54 +0900

    JQuery 비동기 통신

---
+++ 7주차_1/js/ajax.js
@@ -0,0 +1,54 @@
+$(function () {
+    $(".confirm").click(() => {
+        const city = $("input").val();
+        const api_key = "43ef3a569a97482b457ed3a76dc170f5";
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&mode=json&units=metric&appid=${api_key}`;
+    
+        $.ajax({
+            url: url,
+            dataType: "json",
+            type: "GET",
+          })
+            .done((data) => {
+              console.log(data);
+              viewMapping(data);
+            })
+            .fail((error) => {
+              alert(`Error: ${error.status}`);
+            })
+            .always(() => {
+              alert("Complete!");
+            });
+    });
+
+    //날씨 정보 표시 함수
+  function viewMapping(data) {
+    //날씨 아이콘
+    var icon = `<i class="wi wi-owm-${data.weather[0].id}"></i>`;
+    $(".result-wrapper").html(icon);
+
+    //일출시간
+    const sunrise = data.sys.sunrise;
+    const sunriseTime = new Date(sunrise * 1000);
+    console.log("일출시간 : " + sunriseTime);
+
+    //일몰시간
+    const sunset = data.sys.sunset;
+    const sunsetTime = new Date(sunset * 1000);
+    console.log("일몰시간 : " + sunsetTime);
+
+    //날씨 종합 정보 생성
+    const w_info = `
+        <b>Country: </b> ${data.name} <i>${data.sys.country}</i><br/>
+        <b>Current Temperature: </b> ${data.main.temp}&deg;C<br/>
+        <b>Current Humidity: </b> ${data.main.humidity}%<br/>
+        <b>Current Wind Speed: </b> ${data.wind.speed}m/s<br/>
+        <b>Weather Conditions: </b> ${data.weather[0].description}<br/>
+        <b>Clouds: </b> ${data.clouds.all}%<br/>
+        <b>Sunrise: </b> ${sunriseTime}<br/>
+        <b>Sunset: </b> ${sunsetTime}<br/>
+    `;
+    $("#weather-info").html(w_info);
+  } //end of viewMapping
+
+})
---
+++ 7주차_1/js/fetch.js
@@ -0,0 +1,58 @@
+$(function () {
+    $(".confirm").click(() => {
+      const city = $("input").val();
+      const api_key = "43ef3a569a97482b457ed3a76dc170f5";
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&mode=json&units=metric&appid=${api_key}`;
+
+      //fetch API를 이용한 비동기 통신
+      fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((data) => {
+            console.log(data);
+            alert("Success!");
+            viewMapping(data);
+          });
+        } else {
+          throw new Error("Network error");
+        }
+      })
+      .catch((error) => {
+        alert(error);
+        console.log(error);
+      });
+    });
+
+    
+
+    //날씨 정보 표시 함수
+  function viewMapping(data) {
+    //날씨 아이콘
+    var icon = `<i class="wi wi-owm-${data.weather[0].id}"></i>`;
+    $(".result-wrapper").html(icon);
+
+    //일출시간
+    const sunrise = data.sys.sunrise;
+    const sunriseTime = new Date(sunrise * 1000);
+    console.log("일출시간 : " + sunriseTime);
+
+    //일몰시간
+    const sunset = data.sys.sunset;
+    const sunsetTime = new Date(sunset * 1000);
+    console.log("일몰시간 : " + sunsetTime);
+    
+    //날씨 종합 정보 생성
+    const w_info = `
+        <b>Country: </b> ${data.name} <i>${data.sys.country}</i><br/>
+        <b>Current Temperature: </b> ${data.main.temp}&deg;C<br/>
+        <b>Current Humidity: </b> ${data.main.humidity}%<br/>
+        <b>Current Wind Speed: </b> ${data.wind.speed}m/s<br/>
+        <b>Weather Conditions: </b> ${data.weather[0].description}<br/>
+        <b>Clouds: </b> ${data.clouds.all}%<br/>
+        <b>Sunrise: </b> ${sunriseTime}<br/>
+        <b>Sunset: </b> ${sunsetTime}<br/>
+    `;
+    $("#weather-info").html(w_info);
+  } //end of viewMapping
+
+})
